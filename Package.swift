b6549457f7458652010686da9c3182187fd649bb
Steve <stephenlclarke@mac.com>
2026-08-14 06:57:43 +0100

    chore(deps): pin containerization signal fix

--- Package.swift
+++ Package.swift
@@ -66,7 +66,7 @@ let builderShimVersion = ProcessInfo.processInfo.environment["BUILDER_SHIM_VERSI
 let builderShimDigest = ProcessInfo.processInfo.environment["BUILDER_SHIM_DIGEST"] ?? "sha256:c29628471db683a53f4a75bcb759bb9147a65e097ede37c7dde05442afa7518c"
 let scVersion = "0.40.1"
 let containerEngineAPIVersion = Version(0, 3, 5)
-let containerizationRevision = "5423cb7e26b6f1fd78d7f005137efc7b5a776e0b"
+let containerizationRevision = "f0bc99d26cd27ed58b06236421a298d9e4acd5c1"
 let containerEngineAPIRevision = "5e6e24d017691596783515285e1ff56d29701235"
 let scSource =
     ProcessInfo.processInfo.environment["CONTAINERIZATION_SOURCE"]
