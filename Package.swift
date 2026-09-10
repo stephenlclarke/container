ccf99d73b75626ed49a0d638c640bd3b9851e2de
Steve <stephenlclarke@mac.com>
2026-09-10 22:24:00 +0100

    chore(deps): align native Engine API client

--- Package.swift
+++ Package.swift
@@ -67,7 +67,7 @@ let builderShimDigest = ProcessInfo.processInfo.environment["BUILDER_SHIM_DIGEST
 let scVersion = "0.45.0"
 let containerEngineAPIVersion = Version(0, 3, 5)
 let containerizationRevision = "bd8130fea851f6ee264f00fc684e2543a7d2faa3"
-let containerEngineAPIRevision = "84830606abf971110071248e087a80ff4abb86d4"
+let containerEngineAPIRevision = "276a7cfdba91fef60c232177a44c054e5de9ae8f"
 let scSource =
     ProcessInfo.processInfo.environment["CONTAINERIZATION_SOURCE"]
     ?? resolvedPackageLocation(identity: "containerization").map(githubRepositoryPath(from:))
